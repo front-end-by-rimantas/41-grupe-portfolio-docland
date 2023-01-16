ec398b08ceef968295203354e5e7e2ccc0355996
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-16 12:03:14 +0200

    Update

--- js/timer.js
+++ js/timer.js
@@ -5,9 +5,10 @@ const secTill = document.querySelector('.sec-left');
 
 
 const countDown = function () {
-    const todayDate = new Date();
+    const todayDate = new Date().getTime();
     const dateTill = new Date('April 12 2023 00:00:00').getTime();
     const timeGap = dateTill - todayDate;
+    
 
     const sec = 1000;
     const min = sec * 60;
@@ -17,13 +18,13 @@ const countDown = function () {
     const countDay = Math.floor(timeGap / days);
     const countHours = Math.floor((timeGap % days) / h);
     const countMin = Math.floor((timeGap % h) / min);
-    const countSec = Math.floor((timeGap % min)/ sec)
-    console.log(countHours)
+    const countSec = Math.floor((timeGap % min) / sec);
+
 
     daysTill.textContent = `${countDay}`;
     hoursTill.textContent = `${countHours}`;
     minTill.textContent = `${countMin}`;
-    secTill.textContent = `${countSec}`
+    secTill.textContent = `${countSec.toString().padStart(2, 0)}`
 
 }
 
