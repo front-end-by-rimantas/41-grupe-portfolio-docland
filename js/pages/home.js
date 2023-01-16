0e2751d9ccda594d8296061dcfccbe99465b1ea3
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-16 15:29:14 +0200

    footer a href fix

---
+++ js/pages/home.js
@@ -0,0 +1,29 @@
+/* Import */
+
+import { countDown } from "../components/timer/timer.js";
+
+
+
+/* Execution */
+
+/* header start */
+/* header end */
+
+/* hero start */
+/* hero end */
+
+/* timer start */
+countDown();
+/* timer end */
+
+/* section start */
+/* section end */
+
+/* section start */
+/* section end */
+
+/* section start */
+/* section end */
+
+/* footer start */
+/* footer end */
