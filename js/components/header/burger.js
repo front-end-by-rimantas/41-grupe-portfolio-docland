fdbfe843707a9cf726a07c21c0ac2011423d101f
Airidas Juska <airidas665@gmail.com>
2023-01-19 16:03:20 +0200

    moving carret button anim

--- js/components/header/burger.js
+++ js/components/header/burger.js
@@ -4,11 +4,17 @@ const burgerDropdownMenu = document.querySelectorAll('.burger-dropdown');
 const burgerLink = document.querySelectorAll('.burger-link');
 const overlay = document.querySelector('#overlay');
 const exitBtn = document.querySelector('.exit-burger');
+const caretBtn = document.querySelectorAll('.burger-link > .fa-caret-right');
 
 function burger() {
     for (let i = 0; i < burgerLink.length; i++) {
         burgerLink[i].addEventListener('click', function () {
             burgerDropdownMenu[i].classList.toggle('hidden');
+            if (caretBtn[i].classList.contains('caret-right')) {
+                caretBtn[i].classList.replace('caret-right', 'caret-down');
+            } else {
+                caretBtn[i].classList.add('caret-right');
+            }
         });
     }
 
