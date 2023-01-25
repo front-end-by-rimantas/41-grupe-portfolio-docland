f4a125074e684d4c187528d820c10c7c9b593b52
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-25 15:10:48 +0200

    update

--- js/components/carousel/carousel-benefits.js
+++ js/components/carousel/carousel-benefits.js
@@ -3,6 +3,7 @@ const slide = document.querySelector(".slide");
 const prevButton = document.querySelector(".prev-button");
 const nextButton = document.querySelector(".next-button");
 const hiddenTiles = document.querySelectorAll(".benefit-tile");
+const firstActiveTile = document.querySelectorAll(".active-first");
 
 console.log(hiddenTiles)
 
@@ -14,19 +15,27 @@ function carousel() {
      hiddenTiles.forEach(e => e.classList.remove('hidden'))
      
   const slideWidth = slide.clientWidth;
-  console.log(slideWidth)
   slidesContainer.scrollLeft += slideWidth+30;
   
 });
 
 
    prevButton.addEventListener("click", function () {
-       hiddenTiles.forEach(e => e.classList.remove('hidden'))
-        const slideWidth = slide.clientWidth;
-        slidesContainer.scrollLeft -= slideWidth+30;
+      const newSlide = slidesContainer.firstElementChild;
+      slidesContainer.append(newSlide)
+     
+
+      // kodel neveikia??
+      //  newSlide.style.transition = 'all 2s'
+      const slideWidth = newSlide.clientWidth;
+      console.log(slideWidth)
+      slidesContainer.scrollLeft -= slideWidth+30;
         
+   
 });
 
 }
 
-export {carousel}
+export { carousel }
+
+ 
