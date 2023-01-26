1e19e4130d336f0cef41a3e1737b2e1341eaa26c
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-26 13:32:26 +0200

    update

--- js/components/timer/goals-carousel.js
+++ js/components/timer/goals-carousel.js
@@ -1,29 +1,19 @@
-
 const slidesContainer = document.querySelector('.goals-group');
 const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn');
 
 
-
-
 function goalsCarousel() {
-     
 
  nextBtn.addEventListener('click', function () {
-   
    const newElement = slidesContainer.firstElementChild;
    slidesContainer.appendChild(newElement);
-   
   })
 
   prevBtn.addEventListener('click', function () {
-      
     const newElement = slidesContainer.firstElementChild;
     slidesContainer.appendChild(newElement);
-    
   })
  }
 
-
- 
 export { goalsCarousel }
