a1b986e977a042e4d592d2af024812af99dad4a4
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-26 16:53:41 +0200

    update

--- js/components/navigation/sticky-nav.js
+++ js/components/navigation/sticky-nav.js
@@ -1,31 +1,33 @@
-
-const section1 = document.querySelector('.section1')
-
-
 const nav = document.querySelector('.navigation');
 
+const section1= document.querySelector('.section1');
 
 
 const stickyNav = function () {
-
     let scroll = window.pageYOffset;
-    console.log(scroll)
     window.onscroll = function () {
+     
         let currentScroll = window.pageYOffset;
-        if (scroll > currentScroll) {
-console.log(currentScroll)
-            nav.classList.add('sticky');
-        } else if (currentScroll > scroll)  nav.classList.remove('sticky');
-
+        if (currentScroll>scroll ) {
+           nav.classList.remove('sticky');
+        } else {
+           nav.classList.add('sticky');
+        }
         scroll = currentScroll;
     }
 
-
+    
 }
+     
+
+
+    
+// }
 
 
 
 
+   
 
 
 export {stickyNav}
