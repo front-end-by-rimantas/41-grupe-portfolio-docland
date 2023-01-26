97fcca1745389ab3787181b216fbb43793183365
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-26 13:31:48 +0200

    Final

--- js/components/timer/goals-carousel.js
+++ js/components/timer/goals-carousel.js
@@ -1,12 +1,10 @@
 
-const slides = document.querySelector('.goal-tile');
 const slidesContainer = document.querySelector('.goals-group');
 const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn');
-const hiddenTiles= document.querySelectorAll('.hidden-tile')
 
 
- let currentSilde = 0;
+
 
 function goalsCarousel() {
      
@@ -15,24 +13,16 @@ function goalsCarousel() {
    
    const newElement = slidesContainer.firstElementChild;
    slidesContainer.appendChild(newElement);
-      currentSilde++;
-      //  slides.forEach((s) => s.style.transform = `translateX(-100% `);
-       slidesContainer.style.overflow = 'hidden';
+   
   })
 
   prevBtn.addEventListener('click', function () {
       
-      const newElement = slidesContainer.firstElementChild;
-   slidesContainer.appendChild(newElement);
-      currentSilde--;
-        // slides.forEach((s) => s.style.transform = `translateX(+100%`);
-      slidesContainer.style.overflow = 'hidden';
+    const newElement = slidesContainer.firstElementChild;
+    slidesContainer.appendChild(newElement);
+    
   })
  }
-
-
-
-
 
 
  
