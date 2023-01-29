685396de43cf7490178748bcab904ebf5fe9700d
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-29 18:35:48 +0200

    validation

--- js/components/course-instructor/courseInstuctor.js
+++ js/components/course-instructor/courseInstuctor.js
@@ -1,10 +1,20 @@
 const courseInstructor = function (data) {
+
+    if (data.length === 0 || !Array.isArray(data)) {
+        throw new error('This array cannot be empty');
+    }
     
     const courseInstructorDOM = document.querySelector('.course-row');
+    if (courseInstructorDOM === null) {
+        throw error('Cannot find DOM element');
+    }
     
 
     let HTML = '';
     for (const item of data) {
+        if (typeof item !== 'object' || item === null || Array.isArray(item) || !item.header || !item.subHeader || !item.icon1 || !item.icon2 || !item.icon3 || !textIcon1 || !textIcon2 || !textIcon3 || !paragraph || typeof item.icon1 !== 'string' || typeof item.icon2 !=='string'|| typeof item.icon3 !== 'string' || typeof item.textIcon1 !== 'string' || typeof item.textIcon2 !=='string' || typeof item.textIcon3 !== 'string' || typeof item.paragraph!== 'string') {
+            continue;
+         }
         
         HTML += `<div class="col-12 col-lg-6 course-instr-background">
                     <div class="course-in-flex">
